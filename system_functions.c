f3c0793baf0227ea01e66f968b3a087b53ce8c03
Sebastian De la Cruz D <sdelacruz0426@gmail.com>
2023-04-22 00:48:04 -0500

    Codigo listo, solo falta añadir las variables

--- system_functions.c
+++ system_functions.c
@@ -71,22 +71,218 @@ void funcion_inicial (void)
 
 void process_events(void)
 {
-    static bool bandera_blink = FALSE;
+    static bool bandera_blink = FALSE; // Declara una variable boleana estática en FALSE
 
     if(GPIO_getInputPinValue(BSP_BUTTON1_PORT, BSP_BUTTON1) != BOARD_BUTTON_NORMAL_STATE) // Entra cuando se presiona el botón 1.
     {
-        if(bandera_blink == FALSE)
+        i++;//Incrementar la variable i
+
+        if (i==1)//si se presiona el boton 1 una vez
         {
-            bandera_blink = TRUE;
-            GPIO_setOutput(BSP_LED2_PORT,  BSP_LED2,  1);
+            if(bandera_blink == FALSE)
+                   {
+                       bandera_blink = TRUE; // CAMBIA EL ESTADO DE LA VARIABLE ESTATICA
+                       GPIO_setOutput(BSP_LED2_PORT, BSP_LED3, 1); // LED VERDE ON
+                       GPIO_setOutput(BSP_LED2_PORT, BSP_LED2, 0); // LED ROJO OFF
+                       GPIO_setOutput(BSP_LED2_PORT, BSP_LED4, 0); // LED AZUL OFF
+
+                   }
         }
+
 
         else
         {
-            bandera_blink = FALSE;
-            GPIO_setOutput(BSP_LED2_PORT,  BSP_LED2,  0);
+            bandera_blink = FALSE; // REGRESA EL ESTADO DE LA VARIABLE A FALSE
+
         }
+
+        if (i==2)//si se presiona el boton 1 dos veces
+        {
+            if(bandera_blink == FALSE)
+            {
+                bandera_blink = TRUE; // CAMBIA EL ESTADO DE LA VARIABLE ESTATICA
+                GPIO_setOutput(BSP_LED2_PORT, BSP_LED3, 0); // LED VERDE OFF
+                GPIO_setOutput(BSP_LED2_PORT, BSP_LED2, 1); // LED ROJO ON
+                GPIO_setOutput(BSP_LED2_PORT, BSP_LED4, 1); // LED AZUL ON
+            }
+        }
+
+        else
+
+        {
+            bandera_blink = FALSE; // REGRESA EL ESTADO DE LA VARIABLE A FALSE
+        }
+
+        if (i==3)//si se presiona el boton 1 tres veces
+                {
+                    if(bandera_blink == FALSE)
+                    {
+                        bandera_blink = TRUE; // CAMBIA EL ESTADO DE LA VARIABLE ESTATICA
+                        GPIO_setOutput(BSP_LED2_PORT, BSP_LED3, 0); // LED VERDE OFF
+                        GPIO_setOutput(BSP_LED2_PORT, BSP_LED2, 0); // LED ROJO OFF
+                        GPIO_setOutput(BSP_LED2_PORT, BSP_LED4, 1); // LED AZUL ON
+                    }
+                }
+
+                else
+
+                {
+                    bandera_blink = FALSE; // REGRESA EL ESTADO DE LA VARIABLE A FALSE
+                }
+
+        if(i==4)//si se presiona el boton 1 cuatro veces
+        {
+
+            i=0; //se les asigna el valor de cero a las variables i,j con la finalidad de reiniciar la cuenta de ambas
+            j=0;
+
+            //Parpadea 3 veces
+            GPIO_setOutput(BSP_LED2_PORT, BSP_LED3, 0); // LED VERDE OFF
+            GPIO_setOutput(BSP_LED2_PORT, BSP_LED2, 0); // LED ROJO OFF
+            GPIO_setOutput(BSP_LED2_PORT, BSP_LED4, 1); // LED AZUL ON
+            _delay_cycles(5000000);// delay de ciclos
+
+            GPIO_setOutput(BSP_LED2_PORT, BSP_LED3, 0); // LED VERDE OFF
+            GPIO_setOutput(BSP_LED2_PORT, BSP_LED2, 0); // LED ROJO OFF
+            GPIO_setOutput(BSP_LED2_PORT, BSP_LED4, 0); // LED AZUL OFF
+            _delay_cycles(5000000);// delay de ciclos
+
+            GPIO_setOutput(BSP_LED2_PORT, BSP_LED3, 0); // LED VERDE OFF
+            GPIO_setOutput(BSP_LED2_PORT, BSP_LED2, 0); // LED ROJO OFF
+            GPIO_setOutput(BSP_LED2_PORT, BSP_LED4, 1); // LED AZUL ON
+            _delay_cycles(5000000);// delay de ciclos
+
+            GPIO_setOutput(BSP_LED2_PORT, BSP_LED3, 0); // LED VERDE OFF
+            GPIO_setOutput(BSP_LED2_PORT, BSP_LED2, 0); // LED ROJO OFF
+            GPIO_setOutput(BSP_LED2_PORT, BSP_LED4, 0); // LED AZUL OFF
+            _delay_cycles(5000000);// delay de ciclos
+
+            GPIO_setOutput(BSP_LED2_PORT, BSP_LED3, 0); // LED VERDE OFF
+            GPIO_setOutput(BSP_LED2_PORT, BSP_LED2, 0); // LED ROJO OFF
+            GPIO_setOutput(BSP_LED2_PORT, BSP_LED4, 1); // LED AZUL ON
+            _delay_cycles(5000000);// delay de ciclos
+
+            GPIO_setOutput(BSP_LED2_PORT, BSP_LED3, 0); // LED VERDE OFF
+            GPIO_setOutput(BSP_LED2_PORT, BSP_LED2, 0); // LED ROJO OFF
+            GPIO_setOutput(BSP_LED2_PORT, BSP_LED4, 0); // LED AZUL OFF
+
+            }
+
+
+
+
 
         while(GPIO_getInputPinValue(BSP_BUTTON1_PORT, BSP_BUTTON1) != BOARD_BUTTON_NORMAL_STATE);
     }
+
+
+    if(GPIO_getInputPinValue(BSP_BUTTON2_PORT, BSP_BUTTON2) !=BOARD_BUTTON_NORMAL_STATE) // Entra cuando se presiona el botón 2.
+    {
+
+        j++;//Incrementar la variable j
+
+                if (j==1)//si se presiona el boton 2 una vez
+                {
+                    if(bandera_blink == FALSE)
+                           {
+                               bandera_blink = TRUE; // CAMBIA EL ESTADO DE LA VARIABLE ESTATICA
+                               GPIO_setOutput(BSP_LED2_PORT, BSP_LED2, 1); // LED ROJO ON
+                               GPIO_setOutput(BSP_LED2_PORT, BSP_LED3, 0); // LED VERDE OFF
+                               GPIO_setOutput(BSP_LED2_PORT, BSP_LED4, 0); // LED AZUL OFF
+
+
+                           }
+                }
+
+
+                else
+                {
+                    bandera_blink = FALSE; // REGRESA EL ESTADO DE LA VARIABLE A FALSE
+
+                }
+
+                if (j==2)//si se presiona el boton 2 dos veces
+                {
+                    if(bandera_blink == FALSE)
+                    {
+                        bandera_blink = TRUE; // CAMBIA EL ESTADO DE LA VARIABLE ESTATICA
+                        GPIO_setOutput(BSP_LED2_PORT, BSP_LED3, 1); // LED VERDE ON
+                        GPIO_setOutput(BSP_LED2_PORT, BSP_LED4, 1); // LED AZUL ON
+                        GPIO_setOutput(BSP_LED2_PORT, BSP_LED2, 0); // LED ROJO OFF
+
+                    }
+                }
+
+                else
+
+                {
+                    bandera_blink = FALSE; // REGRESA EL ESTADO DE LA VARIABLE A FALSE
+                }
+
+                if (j==3)//si se presiona el boton 2 tres veces
+                        {
+                            if(bandera_blink == FALSE)
+                            {
+                                bandera_blink = TRUE; // CAMBIA EL ESTADO DE LA VARIABLE ESTATICA
+                                GPIO_setOutput(BSP_LED2_PORT, BSP_LED3, 1); // LED VERDE ON
+                                GPIO_setOutput(BSP_LED2_PORT, BSP_LED2, 1); // LED ROJO ON
+                                GPIO_setOutput(BSP_LED2_PORT, BSP_LED4, 0); // LED AZUL OFF
+                            }
+                        }
+
+                        else
+
+                        {
+                            bandera_blink = FALSE; // REGRESA EL ESTADO DE LA VARIABLE A FALSE
+                        }
+
+                if(j==4)//si se presiona el boton 2 cuatro veces
+                {
+
+                                i=0;//se les asigna el valor de cero a las variables i,j con la finalidad de reiniciar la cuenta de ambas
+                                j=0;
+                                //Parpadea 3 veces
+                                GPIO_setOutput(BSP_LED2_PORT, BSP_LED3, 1); // LED VERDE ON
+                                GPIO_setOutput(BSP_LED2_PORT, BSP_LED2, 1); // LED ROJO ON
+                                GPIO_setOutput(BSP_LED2_PORT, BSP_LED4, 0); // LED AZUL OFF
+                                _delay_cycles(5000000);// delay de ciclos
+
+                                GPIO_setOutput(BSP_LED2_PORT, BSP_LED3, 0); // LED VERDE OFF
+                                GPIO_setOutput(BSP_LED2_PORT, BSP_LED2, 0); // LED ROJO OFF
+                                GPIO_setOutput(BSP_LED2_PORT, BSP_LED4, 0); // LED AZUL OFF
+                                _delay_cycles(5000000);// delay de ciclos
+
+                                GPIO_setOutput(BSP_LED2_PORT, BSP_LED3, 1); // LED VERDE ON
+                                GPIO_setOutput(BSP_LED2_PORT, BSP_LED2, 1); // LED ROJO ON
+                                GPIO_setOutput(BSP_LED2_PORT, BSP_LED4, 0); // LED AZUL OFF
+                                _delay_cycles(5000000);// delay de ciclos
+
+                                GPIO_setOutput(BSP_LED2_PORT, BSP_LED3, 0); // LED VERDE OFF
+                                GPIO_setOutput(BSP_LED2_PORT, BSP_LED2, 0); // LED ROJO OFF
+                                GPIO_setOutput(BSP_LED2_PORT, BSP_LED4, 0); // LED AZUL OFF
+                                _delay_cycles(5000000);// delay de ciclos
+
+                                GPIO_setOutput(BSP_LED2_PORT, BSP_LED3, 1); // LED VERDE ON
+                                GPIO_setOutput(BSP_LED2_PORT, BSP_LED2, 1); // LED ROJO ON
+                                GPIO_setOutput(BSP_LED2_PORT, BSP_LED4, 0); // LED AZUL OFF
+                                _delay_cycles(5000000);// delay de ciclos
+
+                                GPIO_setOutput(BSP_LED2_PORT, BSP_LED3, 0); // LED VERDE OFF
+                                GPIO_setOutput(BSP_LED2_PORT, BSP_LED2, 0); // LED ROJO OFF
+                                GPIO_setOutput(BSP_LED2_PORT, BSP_LED4, 0); // LED AZUL OFF
+
+
+
+                }
+
+
+
+
+                while(GPIO_getInputPinValue(BSP_BUTTON2_PORT, BSP_BUTTON2) != BOARD_BUTTON_NORMAL_STATE);
+
+
+
+    }
+
+
 }
